f8b6df2e039f45023fd8059fb1cd760c37a91821
Vincent Verlee <vincent@netlog.com>
2014-10-03 16:04:04 +0200

    add vendor to path

--- src/Composer/Installers/CustomInstaller.php
+++ src/Composer/Installers/CustomInstaller.php
@@ -4,6 +4,6 @@ namespace Composer\Installers;
 class CustomInstaller extends BaseInstaller
 {
 	protected $locations = array(
-		'default'    => 'vendor/{$name}/',
+		'default'    => 'vendor/{$vendor}/{$name}/',
 	);
 }
